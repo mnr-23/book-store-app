54be7a2b490af39d95c6de1776002932a929f0ad
mnr-23 <155671723+mnr-23@users.noreply.github.com>
2025-01-07 15:56:14 +0530

    Delete backend/config.js

--- backend/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 5555;
-export const mongoDBURL =
-  "mongodb+srv://meneninarasimha2003:7kpC5lDQbh1M5omJ@book-store.p4p0h.mongodb.net/books-collection?retryWrites=true&w=majority&appName=book-store";
